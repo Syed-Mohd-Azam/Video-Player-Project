8b5f1b1f387816204937733f2453402f4140ef00
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-13 22:39:13 +0530

    Styling of Search Component Completed

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -3,13 +3,13 @@ const SearchContainer = () => {
   return (
     <>
       <section className="flex justify-center items-center p-10">
-        <article className="flex gap-0 outline outline-1 outline-sky-400 px-7 py-3 justify-center items-center rounded-l-full rounded-r-full lg:hover:w-2/3">
+        <article className="flex gap-0 outline outline-1 outline-sky-400  justify-center items-center rounded-l-full rounded-r-full lg:hover:w-1/2">
           <input
-            className="text-xl italic md:text-2xl lg:text-3xl border-none outline-none basis-3/4 hover:cursor-pointer hover:bg-purple-50"
+            className="text-xl px-7 py-3 italic md:text-2xl lg:text-3xl  outline-none basis-3/5 hover:cursor-pointer hover:bg-purple-50 hover:rounded-l-full  border-r-2 border-sky-400"
             type="search"
             placeholder="Search Video"
           />
-          <LuSearch className="md:w-8 md:h-8 lg:w-12 lg:h-12 hover:cursor-pointer basis-1/4" />
+          <LuSearch className="md:w-10 md:h-10 lg:w-12 lg:h-12 hover:cursor-pointer basis-2/5 px-2 py-2 w-8 h-8" />
         </article>
       </section>
     </>
