aa9f25e7230df8323a5d347d69253ab75b7a4cbe
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-14 02:50:25 +0530

    Styling done

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -5,7 +5,7 @@ const PlayList = ({ videos }) => {
   return (
     <section>
       <article className="py-5 flex justify-center items-center">
-        <h1 className="italic lg:text-5xl text-4xl text-blue-500">
+        <h1 className="italic lg:text-5xl text-4xl text-blue-500 pt-10 pb-5">
           {" "}
           PlayList !!
         </h1>
--- src/components/VideosContainer.jsx
+++ src/components/VideosContainer.jsx
@@ -4,7 +4,7 @@ import VideoCard from "./VideoCard";
 const VideosContainer = ({ videos: { items } }) => {
   return (
     <>
-      <section className="flex flex-wrap gap-8 justify-center items-center pb-20">
+      <section className="flex flex-wrap gap-8 justify-center items-center pb-20 pt-10">
         {items.map((item) => (
           <VideoCard key={item?.id} item={item} />
         ))}
