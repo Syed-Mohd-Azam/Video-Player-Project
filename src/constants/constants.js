e0215ff720da46bb6f6c3a787c52a32ea14791b4
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-14 01:07:15 +0530

    YOUTUBE_API_KEY and YOUTUBE_API stored

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+const YOUTUBE_API_kEY = "AIzaSyD5o_WlNFauPuRtVsZIKOm9LABtFJwDYtk";
+export const YOUTUBE_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&regionCode=us&key=" +
+  YOUTUBE_API_kEY;
