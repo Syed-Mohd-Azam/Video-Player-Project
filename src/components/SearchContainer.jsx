a06d5482959d595d5137076979c3d03ee90e4e71
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-13 22:27:26 +0530

    Styling to SearchContainer Component done

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -5,7 +5,7 @@ const SearchContainer = () => {
       <section className="flex justify-center items-center p-10">
         <article className="flex gap-0 outline outline-1 outline-sky-400 px-7 py-3 justify-center items-center rounded-l-full rounded-r-full lg:hover:w-2/3">
           <input
-            className="text-xl italic md:text-2xl lg:text-3xl border-none outline-none basis-3/4"
+            className="text-xl italic md:text-2xl lg:text-3xl border-none outline-none basis-3/4 hover:cursor-pointer hover:bg-purple-50"
             type="search"
             placeholder="Search Video"
           />
