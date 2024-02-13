e4f4338e1b1accf5cf2631b52fc4d3cdc092f23c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-13 21:37:43 +0530

    Search Component Completed

---
+++ src/components/PlaylistComponent.jsx
@@ -0,0 +1,12 @@
+import SearchContainer from "./SearchContainer";
+
+const PlaylistComponent = () => {
+  return (
+    <>
+      <section className="w-full">
+        <SearchContainer />
+      </section>
+    </>
+  );
+};
+export default PlaylistComponent;
---
+++ src/components/SearchContainer.jsx
@@ -0,0 +1,18 @@
+import { LuSearch } from "react-icons/lu";
+const SearchContainer = () => {
+  return (
+    <>
+      <section className="flex justify-center items-center p-10">
+        <article className="flex gap-0 outline outline-1 outline-sky-400 px-7 py-3 justify-center items-center rounded-l-full rounded-r-full lg:hover:w-2/3">
+          <input
+            className="text-xl italic md:text-2xl lg:text-3xl border-none outline-none basis-3/4"
+            type="search"
+            placeholder="Search Video"
+          />
+          <LuSearch className="md:w-8 md:h-8 lg:w-12 lg:h-12 hover:cursor-pointer basis-1/4" />
+        </article>
+      </section>
+    </>
+  );
+};
+export default SearchContainer;
