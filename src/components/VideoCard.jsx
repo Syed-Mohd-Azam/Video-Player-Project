e6e385ade304e9b7b0562850266cc4e1353d5bca
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-14 02:44:58 +0530

    styling of VideoCards done

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,10 +21,10 @@ const VideoCard = ({
           />
         </article>
         <article className="w-full h-1/2 px-5 py-3 bg-purple-50 rounded-xl flex flex-col  justify-center">
-          <h1 className="pb-2 italic text-lg md:text-xl lg:text-2xl font-semibold">
+          <h1 className="pb-2 italic text-md md:text-xl lg:text-2xl font-semibold">
             {channelTitle}
           </h1>
-          <h1 className=" italic md:text-md lg:text-lg">{title}</h1>
+          <h1 className="text-sm italic md:text-md lg:text-lg">{title}</h1>
         </article>
       </section>
     </>
--- src/components/VideosContainer.jsx
+++ src/components/VideosContainer.jsx
@@ -4,7 +4,7 @@ import VideoCard from "./VideoCard";
 const VideosContainer = ({ videos: { items } }) => {
   return (
     <>
-      <section className="flex flex-wrap gap-8">
+      <section className="flex flex-wrap gap-8 justify-center items-center">
         {items.map((item) => (
           <VideoCard key={item?.id} item={item} />
         ))}
