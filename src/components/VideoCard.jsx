a02496e3825ac8ff9b69e4ca3f7793276399b075
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-14 02:00:46 +0530

    key attribute added to the VideoCard Component during map

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,8 @@
+const VideoCard = () => {
+  return (
+    <>
+      <section></section>
+    </>
+  );
+};
+export default VideoCard;
--- src/components/VideosContainer.jsx
+++ src/components/VideosContainer.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
+import VideoCard from "./VideoCard";
 const VideosContainer = ({ videos: { items } }) => {
   return (
     <>
-      <section>{items.map((item) => console.log(item))}</section>
+      <section>
+        {items.map((item) => (
+          <VideoCard key={item?.id} item={item} />
+        ))}
+      </section>
     </>
   );
 };
