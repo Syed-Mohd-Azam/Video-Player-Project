60ad153561b7458efb0217d29b10682cee93da90
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-14 02:42:23 +0530

    Videos Displayed in the Main Container

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -12,17 +12,19 @@ const VideoCard = ({
 }) => {
   return (
     <>
-      <section className="w-80 h-auto border-1 ">
-        <article>
+      <section className="  gap-3 w-80 h-96 mt-6">
+        <article className="w-full h-1/2">
           <img
-            className="w-full h-2/5 rounded-xl hover:cursor-pointer"
+            className=" rounded-xl hover:cursor-pointer"
             src={url}
             alt="Video-Image"
           />
         </article>
-        <article className="w-full h-3/5 p-3">
-          <h1>{channelTitle}</h1>
-          <h1>{title}</h1>
+        <article className="w-full h-1/2 px-5 py-3 bg-purple-50 rounded-xl flex flex-col  justify-center">
+          <h1 className="pb-2 italic text-lg md:text-xl lg:text-2xl font-semibold">
+            {channelTitle}
+          </h1>
+          <h1 className=" italic md:text-md lg:text-lg">{title}</h1>
         </article>
       </section>
     </>
