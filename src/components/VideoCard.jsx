d76b525b1f86373fb17b643475550738d09a0797
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-15 22:20:15 +0530

    Project Almost Completed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,13 +19,13 @@ const VideoCard = ({
         <article className="w-full h-1/2">
           <Link to={"/watch?v=" + id}>
             <img
-              className=" rounded-xl hover:cursor-pointer"
+              className=" rounded-xl hover:cursor-pointer hover:scale-90"
               src={url}
               alt="Video-Image"
             />
           </Link>
         </article>
-        <article className="w-full h-1/2 px-5 py-3 bg-purple-50 rounded-xl flex flex-col  justify-center hover:scale-110">
+        <article className="w-full h-1/2 px-5 py-3 bg-purple-50 rounded-xl flex flex-col  justify-center hover:scale-110 hover:cursor-pointer">
           <h1 className="pb-2 italic text-md md:text-xl lg:text-2xl font-semibold">
             {channelTitle}
           </h1>
